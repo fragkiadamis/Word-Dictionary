f0976099eeec6a83944d2be2d96de47b797038b3
Adam Fragkiadakis <adamfrag@gmail.com>
2022-01-06 22:19:54 +0200

    Add one word in the dictionary (word's column in the file is pending)

--- main.c
+++ main.c
@@ -6,6 +6,25 @@
 
 #define HASH_TABLE_SIZE 1024
 
+typedef struct position_node {
+    int row;
+    int col;
+    struct position_node *next;
+} POS_NODE;
+
+typedef struct file_node {
+    char *name;
+    struct word_node *next;
+    POS_NODE *position;
+} FILE_NODE;
+
+typedef struct word_node {
+    char *word;
+    struct word_node *left;
+    struct word_node *right;
+    FILE_NODE *file;
+} WORD_NODE;
+
 // Hashing function, calculates the position of a word in the hash table;
 int hashingFunction(char *word, int word_length) {
     int sum = 0;
@@ -20,11 +39,58 @@ int hashingFunction(char *word, int word_length) {
     return sum % HASH_TABLE_SIZE;
 }
 
+// add the position of the word in the file that is coming from
+void addFilePosition(POS_NODE **filePosition, int row, int col) {
+    // Add word's position in respective file
+    printf("%d\n", row);
+    *filePosition = (POS_NODE*)malloc(sizeof(POS_NODE));
+    (**filePosition).row = row;
+    (**filePosition).col = col;
+}
+
+// Add the file in the dictionary that the word is coming from
+void addFile(FILE_NODE **fileNode, char *fileName, int row, int col){
+    // Add word's respective filename
+    *fileNode = (FILE_NODE*)malloc(sizeof(FILE_NODE));
+    (*fileNode)->name = (char*)malloc(strlen(fileName) * sizeof(char));
+    strcpy((*fileNode)->name, fileName);
+
+    // Add the word's position in the file from which it is coming from
+    addFilePosition(&((*fileNode)->position), row, col);
+}
+
+// Add the word in the dictionary
+void addWord(WORD_NODE **wrdNode, char *word, int wordLength, char *fileName, int row, int col) {
+    // Add word
+    *wrdNode = (WORD_NODE*)malloc(sizeof(WORD_NODE));
+    (*wrdNode)->word = (char*)malloc(wordLength * sizeof(char));
+    strcpy((*wrdNode)->word, word);
+
+    // Add the file from which the word is coming from
+    addFile(&(*wrdNode)->file, fileName, row, col);
+}
+
+void addInDictionary(WORD_NODE **wrdNode, char *word, int wordLength, char *fileName, int row, int col) {
+    // Word does not exist in dictionary, add the word to dictionary
+    if (*wrdNode == NULL)
+        addWord(wrdNode, word, wordLength, fileName, row, col);
+    else {
+        int wordCmp = strcmp((*wrdNode)->word, word);
+        if (wordCmp > 0) {
+
+        } else if (wordCmp < 0) {
+
+        } else {
+
+        }
+    }
+}
+
 int main(void) {
     char ch, *path = "/home/fragkiadamis/CLionProjects/Assignment_2/files/";
     struct dirent *de;
     FILE *fptr;
-    char *hash[HASH_TABLE_SIZE];
+    WORD_NODE *hashTable[HASH_TABLE_SIZE] = {NULL};
 
     // Open directory, terminate if the process fails.
     DIR *dr = opendir(path);
@@ -47,19 +113,30 @@ int main(void) {
             strcat(strcat(filePath, path), de->d_name);
             fptr = fopen(filePath, "r");
 
+            // Declare variables to store row and column of the word in the file
+            int row = 1, col = 1;
+
             // Create a pointer to store each word
             char *word = (char*)malloc(2 * sizeof(char));
             int bytes_allocated = 0;
 
             // While it's not the End Of File...
             // Isolate each word. Words are distinguished by either a space or a newline
-            while((ch = fgetc(fptr)) != EOF)
-            {
-                if(ch == '\n' || ch == ' ') // The word is over
-                {
+            while((ch = fgetc(fptr)) != EOF) {
+                if(ch == '\n' || ch == ' ') { // The word is over
+                    if (ch == '\n') {
+                        row++; // Increase row
+                        col = 1; // Default column
+                    }
+
                     word[bytes_allocated] = '\0'; // Add null terminator
-                    int pos = hashingFunction(word, strlen(word)); // Find the word's position in the hash table.
-                    printf("word: %s, pos: %d\n", word, pos);
+                    int wordLength = strlen(word);
+                    int pos = hashingFunction(word, wordLength); // Find the word's position in the hash table.
+                    addInDictionary(&hashTable[pos], word, wordLength, de->d_name, row, col);
+                    printf("word: %s, ", hashTable[pos]->word);
+                    printf("fileName: %s, ", hashTable[pos]->file->name);
+                    printf("row: %d col: %d\n", hashTable[pos]->file->position->row, hashTable[pos]->file->position->col);
+                    break;
 
                     // Default the word and continue to next one.
                     word = (char*)malloc(2 * sizeof(char));
@@ -72,6 +149,7 @@ int main(void) {
             }
             free(word);
             fclose(fptr);
+            break;
         }
     }
     closedir(dr);
